16930544b1c266b484d7b00e92a9b8bfae4ed426
Kiun-Kai <kiunkaims@gmail.com>
2024-12-05 14:35:17 +0300

    Add files via upload

---
+++ Indeksacia.py
@@ -0,0 +1,13 @@
+example="Милисса"
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
+
+example="Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
